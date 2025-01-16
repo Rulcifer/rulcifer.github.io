6facd2b3d5d60d4aa506b8cf54c02ab8f5ff306b
Rucifer <sahrulrafi7@gmail.com>
2025-01-16 20:16:13 +0700

    update contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import './Contact.css';
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => (
   <div className="contact" id="contact">
     <div className="footer-left">
       <h1>
-        I LOVE
-        {' '}
-        <br />
+        I LOVE <br />
         COFFEE
-        <br />
-        {' '}
-        CHAT ☕️
+        <br /> CHAT ☕️
       </h1>
     </div>
     <div className="footer-right">
@@ -19,7 +15,7 @@ const Contact = () => (
       <div className="interests">
         <p>Frontend Development</p>
         <p>Backend Development</p>
-        <p>React</p>
+        <p>JavaScript</p>
         <p>WordPress Development</p>
         <p>Startups</p>
         <p>New Opportunities</p>
@@ -32,16 +28,32 @@ const Contact = () => (
 
       <h3>
         MINDING A PROJECT?
-        <span><a href="mailto:sahrulrafizulfitra@gmail.com">Contact Me</a></span>
+        <span>
+          <a href="mailto:sahrulrafizulfitra@gmail.com">Contact Me</a>
+        </span>
       </h3>
 
       <hr />
 
       <div className="social">
-        <span><a href="https://github.com/Rulcifer" target="_blank" rel="noreferrer">GITHUB</a></span>
         <span>
-          {' '}
-          <a href="https://www.linkedin.com/in/sahrul-rafi-zulfitra-7a3bb6172/" target="_blank" rel="noreferrer">LINKEDIN</a>
+          <a
+            href="https://github.com/Rulcifer"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GITHUB
+          </a>
+        </span>
+        <span>
+          {" "}
+          <a
+            href="https://www.linkedin.com/in/sahrul-rafi-zulfitra-7a3bb6172/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LINKEDIN
+          </a>
         </span>
       </div>
     </div>
