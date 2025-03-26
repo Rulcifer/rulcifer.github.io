35874f2ed8e6bc70a214b026334b5c8044786988
Rucifer <rulcifers@gmail.com>
2025-03-26 21:35:47 +0700

    udpate navbar responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import './NavBar.css';
-import { Link } from 'react-scroll';
+import React, { useEffect, useState } from "react";
+import "./NavBar.css";
+import { Link } from "react-scroll";
 
 const NavBar = () => {
   const [navbarStyle, setNavbarStyle] = useState({});
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
   const [scroll, setScroll] = useState(false);
 
   const toggleMenu = () => {
@@ -14,15 +14,15 @@ const NavBar = () => {
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY || document.documentElement.scrollTop;
-      const scrollHeight = document.documentElement.scrollHeight
-      - document.documentElement.clientHeight;
+      const scrollHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
       const scrollPercentage = (scrollTop / scrollHeight) * 100;
 
       if (scrollPercentage >= 2) {
         setScroll(true);
-
         setNavbarStyle({
-          backgroundColor: 'rgba(0, 0, 0, 0.2)',
+          backgroundColor: "rgba(0, 0, 0, 0.2)",
         });
       } else {
         setNavbarStyle({});
@@ -30,53 +30,90 @@ const NavBar = () => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-
     <>
-      <nav style={navbarStyle} className={`navbar ${open ? 'nav-open' : ''} ${scroll ? 'scrolled' : ''}`}>
+      <nav style={navbarStyle} className={`navbar ${scroll ? "scrolled" : ""}`}>
         <div className="tophead">
           <h1>
-            <Link className="name-logo" spy smooth offset={50} duration={500} to="home">
+            <Link
+              className="name-logo"
+              spy
+              smooth
+              offset={50}
+              duration={500}
+              to="home"
+            >
               sahrul rafi
             </Link>
-            {' '}
             <span className="blink" />
           </h1>
-          <div className={`menu-btn ${open ? 'opened-btn' : ''}`} onClick={toggleMenu} />
+          <div
+            className={`menu-btn ${open ? "opened-btn" : ""}`}
+            onClick={toggleMenu}
+          />
         </div>
-        <ul className={`menu ${open ? 'open' : ''}`}>
-
+        <ul className={`menu ${open ? "open" : ""}`}>
           <li>
-            <Link spy smooth offset={50} duration={500} onClick={toggleMenu} to="home">
-              home,
+            <Link
+              spy
+              smooth
+              offset={50}
+              duration={500}
+              onClick={toggleMenu}
+              to="home"
+            >
+              <i className="fa-solid fa-house"></i>
+              <span>home</span>
             </Link>
           </li>
           <li>
-            <Link spy smooth offset={50} duration={500} onClick={toggleMenu} to="about">
-              about,
+            <Link
+              spy
+              smooth
+              offset={50}
+              duration={500}
+              onClick={toggleMenu}
+              to="about"
+            >
+              <i className="fa-solid fa-user"></i>
+              <span>about</span>
             </Link>
           </li>
           <li>
-            <Link spy smooth offset={50} duration={500} onClick={toggleMenu} to="projects">
-              projects,
+            <Link
+              spy
+              smooth
+              offset={50}
+              duration={500}
+              onClick={toggleMenu}
+              to="projects"
+            >
+              <i className="fa-solid fa-folder"></i>
+              <span>projects</span>
             </Link>
           </li>
           <li>
-            <li>
-              <Link spy smooth offset={50} duration={500} onClick={toggleMenu} to="contact">
-                contact
-              </Link>
-            </li>
+            <Link
+              spy
+              smooth
+              offset={50}
+              duration={500}
+              onClick={toggleMenu}
+              to="contact"
+            >
+              <i className="fa-solid fa-envelope"></i>
+              <span>contact</span>
+            </Link>
           </li>
         </ul>
       </nav>
-      <div className={`scroll-to-top ${scroll ? 'scroll-true' : ''}`}>
+      <div className={`scroll-to-top ${scroll ? "scroll-true" : ""}`}>
         <Link spy smooth offset={50} duration={1000} to="home">
           <i className="fa-solid fa-arrow-up" />
         </Link>
